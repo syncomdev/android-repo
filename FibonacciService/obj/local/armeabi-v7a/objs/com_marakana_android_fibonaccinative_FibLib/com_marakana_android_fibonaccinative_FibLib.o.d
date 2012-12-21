8922d040e0093a3835f66ebf9c4d58dcb66565aa
Ning Shang <geek@cerias.net>
2012-12-20 23:51:05 -0800

    Initial import of the existing files.

---
+++ FibonacciService/obj/local/armeabi-v7a/objs/com_marakana_android_fibonaccinative_FibLib/com_marakana_android_fibonaccinative_FibLib.o.d
@@ -0,0 +1,8 @@
+obj/local/armeabi-v7a/objs/com_marakana_android_fibonaccinative_FibLib/com_marakana_android_fibonaccinative_FibLib.o: \
+ jni/com_marakana_android_fibonaccinative_FibLib.c \
+ jni/com_marakana_android_fibonaccinative_FibLib.h \
+ /Developer/Android/ndk/platforms/android-8/arch-arm/usr/include/jni.h
+
+jni/com_marakana_android_fibonaccinative_FibLib.h:
+
+/Developer/Android/ndk/platforms/android-8/arch-arm/usr/include/jni.h:
---
+++ FibonacciService/obj/local/x86/objs/com_marakana_android_fibonaccinative_FibLib/com_marakana_android_fibonaccinative_FibLib.o.d
@@ -0,0 +1,8 @@
+obj/local/x86/objs/com_marakana_android_fibonaccinative_FibLib/com_marakana_android_fibonaccinative_FibLib.o: \
+ jni/com_marakana_android_fibonaccinative_FibLib.c \
+ jni/com_marakana_android_fibonaccinative_FibLib.h \
+ /Developer/Android/ndk/platforms/android-9/arch-x86/usr/include/jni.h
+
+jni/com_marakana_android_fibonaccinative_FibLib.h:
+
+/Developer/Android/ndk/platforms/android-9/arch-x86/usr/include/jni.h:
